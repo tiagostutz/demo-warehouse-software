8a92ecb51eb5633ec4c36b335579323f21c6862f
Tiago Stutz <tiagostutz@gmail.com>
2021-07-11 14:18:49 -0300

    setup: api-backend typescript configurated with prisma, express, logging, lint and a basic routing using prisma. closes #1 closes #2

--- api-backend/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from 'tslog';
-
-export const log = new Logger({
-  displayFilePath: 'hidden',
-  displayFunctionName: false,
-  displayInstanceName: false,
-  colorizePrettyLogs: true,
-});
